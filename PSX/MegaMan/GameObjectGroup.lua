576a347bbcaad27dbf779171116b2d086756a712
Hyago Oliveira <hyagogow@gmail.com>
2024-12-10 19:05:36 -0300

    Add GameObjects array

--- PSX/MegaMan/GameObjectGroup.lua
+++ PSX/MegaMan/GameObjectGroup.lua
@@ -14,27 +14,40 @@ function GameObjectGroup:initialize(name, address)
     self.name = name
     self.address = address
     self.showProperties = false
+    self.gameObjects = {}
     self.disabled = MemoryAddress:new("Bool", address.disabled)
 end
 
 function GameObjectGroup:update()
     self.disabled:update()
+    self:updateGameObjects()
 end
 
 function GameObjectGroup:draw()
+    for _, go in pairs(self.gameObjects) do
+        go:draw()
+
+        if self.showProperties then
+            go:printPropertiesOverRight()
+        end
+    end
+end
+
+function GameObjectGroup:updateGameObjects()
     for i = 0, self.address.max - 1 do
         local current = self.address.base + i * self.address.blockSize
         local isEnabled = MemoryAddress.READ["Bool"](current)
 
         if isEnabled then
-            local name = string.format("%s %s", self.name, i)
-            local go = GameObject:new(current, name)
-
-            go:update()
-            go:draw()
-            if self.showProperties then
-                go:printPropertiesOverRight()
+            if self.gameObjects[i] == nil then
+                local name = string.format("%s %s", self.name, i)
+                self.gameObjects[i] = GameObject:new(current, name)
             end
+
+            self.gameObjects[i]:update()
+            self:updateFrozenProprieties(self.gameObjects[i])
+        else
+            self.gameObjects[i] = nil
         end
     end
 end
@@ -72,6 +85,9 @@ end
 
 function GameObjectGroup:disable()
     self.disabled:write(true)
+end
+
+function GameObjectGroup:updateFrozenProprieties(gameObject)
 end
 
 return GameObjectGroup
